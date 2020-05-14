873de09ec942379dc964837c3ebd7fbec58da323
Ruslan04071996 <ruslan040796@mail.ru>
2020-05-14 15:17:48 +0300

    Полностью сделал первый свипер

--- src/js/index.js
+++ src/js/index.js
@@ -3,4 +3,5 @@ import  'swiper/js/swiper.js';
 import 'swiper/swiper.scss';
 import './popup.js';
 import './modal.js';
+import './swiper.js';
 console.log('Works!');
---
+++ src/js/swiper.js
@@ -0,0 +1,2 @@
+let paginationLogoEl = document.querySelector('.swiper-pagination_logo');
+paginationLogoEl.style.bottom = '-56px';
