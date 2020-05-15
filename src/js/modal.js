dc758b38244db4c56d51b0b1fd56caa20101fa95
Ruslan04071996 <ruslan040796@mail.ru>
2020-05-15 15:40:59 +0300

    Сверстал второе модальное окно

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,2 +1,12 @@
 const modalEl = document.querySelector('.modal');
 modalEl.style.display = 'none';
+
+const commercialLink = document.querySelector('.warn__link');
+const modalHeaderEl = document.querySelector('.modal__promo-text');
+const modalFeedbackInput = document.querySelector('.modal__input_hidden');
+
+commercialLink.addEventListener('click', function (ev) {
+    ev.preventDefault();
+    modalHeaderEl.textContent = 'Обратная связь';
+    modalFeedbackInput.style.display = 'block';
+})
