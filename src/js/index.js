8805b476308a8c3f48b2645c3be1bf9b423deb37
Ruslan04071996 <ruslan040796@mail.ru>
2020-05-17 22:01:40 +0300

    Готова полеостью мобильная версия с 2мя косяками

--- src/js/index.js
+++ src/js/index.js
@@ -5,10 +5,3 @@ import './popup.js';
 import './modal.js';
 import './swiper.js';
 console.log('Works!');
-// Временно
-document.querySelector('.content').style.display = 'none';
-document.querySelector('#swiper1').style.display = 'none';
-document.querySelector('#swiper2').style.display = 'none';
-document.querySelector('.head').style.display = 'none';
-document.querySelector('.head2').style.display = 'none';
-document.querySelector('.head3').style.display = 'none';
